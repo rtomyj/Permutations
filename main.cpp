d0897a26f6e90ecc3fbc735a2fcbbd2f30aac692
Javi Gomez <rtomyj@gmail.com>
2017-03-25 22:16:07 -0500

    updated main class

--- main.cpp
+++ main.cpp
@@ -1,25 +1,21 @@
-//
-//  main.cpp
-//  Permutations
-//
-//  Created by Javi Gomez on 3/16/17.
-//  Copyright © 2017 Javi Gomez. All rights reserved.
-//
+/*
+ V 1.0
+ Class Function : basic usablilty of Permutation class. Should be used as a reference to how the class works
+ 
+ */
 
 #include <iostream>
 #include"Permutation.hpp"
-
 using namespace std;
-
-void permute(string);
+// creates two string instances with bird and cat as their value. Then creates two permutaion instances passing the two strings, 1 to each permute
+//instance
 int main(int argc, const char * argv[]) {
-    
-    
-    string x ;
-    x.append("birds");
-    Permute y(x) ;
-   // y.print();
-    y.printPermutations();
+    string word1, word2 ;
+    word1.append("BIRD");
+    word2.append("CAT");
+    Permute permutation1(word1), permutation2(word2) ;
+    permutation1.print();
+    permutation1.printPermutations();
     return 0;
 }
 
